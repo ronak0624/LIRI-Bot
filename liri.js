14afc5ae8e71c83b79e11e38e8eedb21152ebd5f
Ronak Patel <ronak0624@gmail.com>
2019-09-26 15:08:03 -0700

    concert working

--- liri.js
+++ liri.js
@@ -1,15 +1,60 @@
-
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
-var spotify = new Spotify(keys.spotify);
+var nspot = require("node-spotify-api");
 
-function concertThis(){
-    let artist = process.argv[2]
+// var spotify = new Spotify(keys.spotify);
+
+function concertThis(artist) {
+    // let artist = process.argv.slice(2).join(" ")
+    console.log(artist)
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    axios.get(queryURL).then(function(response){
-        console.log(response)
+    axios.get(queryURL).then(function (response) {
+        let selectedEvent = response.data[0]
+        console.log("Event Name: ", selectedEvent.venue.name);
+        console.log("Location: ", selectedEvent.venue.city, ",", selectedEvent.venue.region);
+        console.log("Date: ", selectedEvent.datetime);
+
+    }).catch(function (err) {
+        throw err;
     });
 }
 
-concertThis()
+function movieThis(movie) {
+    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryURL).then(
+            function (response) {
+                var movieInfo = response.data
+                //     Title of the movie.
+                console.log(movieInfo);
+                //   * Year the movie came out.
+                //   * IMDB Rating of the movie.
+                //   * Rotten Tomatoes Rating of the movie.
+                //   * Country where the movie was produced.
+                //   * Language of the movie.
+                //   * Plot of the movie.
+                //   * Actors in the movie.
+            })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+movieThis("titanic");
